3a9130b731e469c398639050cee79a0ed96fe6cf
luiz <lguilhermefl@gmail.com>
2023-01-17 18:58:20 -0300

    feat: save data from request

--- src/main/java/com/cars/api/controller/CarController.java
+++ src/main/java/com/cars/api/controller/CarController.java
@@ -1,20 +1,23 @@
 package com.cars.api.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.cars.api.dto.CarDTO;
+import com.cars.api.repository.CarRepository;
 
 @RestController
 @RequestMapping("/api/cars")
 public class CarController {
 
+    @Autowired
+    private CarRepository repository;
+
     @PostMapping
     public void create(@RequestBody CarDTO req) {
-        System.out.println(req);
-        System.out.println(req.modelo());
-        System.out.println(req.valor());
+        repository.save(req);
     }
 }
